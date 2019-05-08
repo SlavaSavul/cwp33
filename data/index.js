805f863d03e3a0da1aaf505a0cd2bdd5f53ea184
SlavaSavul <slavik_savul@mail.ru>
2019-05-08 14:43:22 +0300

    1

---
+++ data/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const config = require('../config.json');
+
+const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
+
+const Repo = require('./repo')(Sequelize, sequelize);
+const Commit = require('./commit')(Sequelize, sequelize);
+
+Commit.belongsTo(Repo, { foreignKey: 'repoId' });
+
+const context = {
+    repos: Repo,
+    commits: Commit,
+
+    Sequelize,
+    sequelize,
+};
+
+module.exports = context;
